3b4920ce1d954a0b0f2bf2f18fdee0d9f0bdeb96
oxygen- <dmizgeev@mail.ru>
2024-07-17 14:54:24 +0300

    first-iteration

--- ftracker.go
+++ ftracker.go
@@ -100,7 +100,7 @@ const (
 // weight float64 — вес пользователя.
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
-    return ((walkingCaloriesWeightMultiplier * weight + (math.Pow(meanSpeed(action, duration), 2) / (height / 100)) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
+    return  ((walkingCaloriesWeightMultiplier * weight + (math.Pow(meanSpeed(action, duration), 2) / (height / 100)) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
